acf1f9d68685214094f37c2ab598cb26bf7cd95c
rdasilva01 <rodrigods2001@gmail.com>
2025-05-09 14:20:15 +0200

    Create test and fix wrapper

--- ekf_wrapper.py
+++ ekf_wrapper.py
@@ -9,26 +9,47 @@ class EKFWrapper:
 
     def __init__(self,
                  initial_state: np.ndarray,
-                 initial_covariance_diagonal: np.ndarray,
+                 initial_covariance: np.ndarray,
                  imu_noise: np.ndarray,
                  process_noise_covariance: np.ndarray):
         """
         Initialize the EKF.
 
         :param initial_state (np.ndarray): The initial state vector.
-
+        :param initial_covariance (np.ndarray): The initial covariance.
+        :param imu_noise (np.ndarray): The IMU noise vector (accelerometer x, y, z and gyroscope x, y, z).
+        :param process_noise_covariance (np.ndarray): The process noise covariance matrix.
         """
         self.ekf = EKF()
         self.state = initial_state
-        self.state_covariance = initial_covariance_diagonal
+        self.state_covariance = initial_covariance
         self.process_noise_covariance = process_noise_covariance
         self.imu_noise = imu_noise
+
+    def get_state(self) -> np.ndarray:
+        """
+        Get the current state.
+
+        :return: The current state vector.
+        """
+        return self.state
+
+    def get_state_covariance(self) -> np.ndarray:
+        """
+        Get the current state covariance.
+
+        :return: The current state covariance matrix.
+        """
+        return self.state_covariance
 
     def predict(self,
                 imu_measurement: np.ndarray,
                 dt: float):
         """
         Predict the next state.
+
+        :param imu_measurement (np.ndarray): The IMU measurement vector.
+        :param dt (float): The time step.
         """
         X_new, P_new = self.ekf.predict_function(
             self.state,
@@ -48,6 +69,7 @@ class EKFWrapper:
         Update the state with a new measurement.
 
         :param z (np.ndarray): The measurement vector.
+        :param measurement_noise_covariance (np.ndarray): The measurement noise covariance matrix.
         """
         X_new, P_new = self.ekf.update_function(
             self.state,
@@ -57,11 +79,3 @@ class EKFWrapper:
         )
         self.state = X_new
         self.state_covariance = P_new
-
-
-def main():
-    print("EKF Test")
-
-
-if __name__ == "__main__":
-    main()
---
+++ test.py
@@ -0,0 +1,70 @@
+import numpy as np
+from ekf_wrapper import EKFWrapper
+
+
+def main():
+    print("Test for the EKF wrapper.")
+
+    # Example parameters
+    initial_state = np.array([
+        0.0, 0.0, 0.0,  # Position (x, y, z)
+        0.0, 0.0, 0.0,  # Velocity (vx, vy, vz)
+        1.0, 0.0, 0.0, 0.0,  # Orientation (quaternion w, x, y, z)
+        0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0
+    ])
+    initial_covariance = np.zeros((16, 16))
+    imu_noise = np.array([
+        0.0, 0.0, 0.0,    # Accelerometer noise
+        0.0, 0.0, 0.0  # Gyroscope noise
+    ])
+    process_noise_covariance_diagonal = np.array([
+        0.0, 0.0, 0.0,     # Accelerometer noise covariance
+        0.0, 0.0, 0.0,  # Gyroscope noise covariance
+    ])
+
+    print("Initial state:", initial_state)
+    print("Initial covariance diagonal:", initial_covariance)
+    print("IMU noise:", imu_noise)
+    print("Process noise covariance:", process_noise_covariance_diagonal)
+
+    ekf_wrapper = EKFWrapper(
+        initial_state,
+        initial_covariance,
+        imu_noise,
+        process_noise_covariance_diagonal)
+
+    print("EKF wrapper initialized.")
+
+    # Example IMU Measurement
+    imu_measurement = np.array([
+        0.0, 0.0, 10.81,  # Accelerometer (ax, ay, az)
+        0.0, 0.0, 0.0  # Gyroscope (gx, gy, gz)
+    ])
+    dt = 0.01
+    ekf_wrapper.predict(imu_measurement, dt)
+    ekf_wrapper.predict(imu_measurement, dt)
+    ekf_wrapper.predict(imu_measurement, dt)
+    ekf_wrapper.predict(imu_measurement, dt)
+    ekf_wrapper.predict(imu_measurement, dt)
+
+    print("State after prediction:", ekf_wrapper.get_state())
+    print("State covariance after prediction:",
+          ekf_wrapper.get_state_covariance())
+
+    # Example pose Measurement
+    pose_measurement = np.array([
+        0.0, 0.0, 0.0,  # Position (x, y, z)
+        1.0, 0.0, 0.0, 0.0  # Orientation (quaternion w, x, y, z)
+    ])
+    pose_covariance = np.eye(7) * 0.0
+
+    ekf_wrapper.update(pose_measurement, pose_covariance)
+
+    print("State after update:", ekf_wrapper.get_state())
+    print("State covariance after update:",
+          ekf_wrapper.get_state_covariance())
+
+
+if __name__ == "__main__":
+    main()
